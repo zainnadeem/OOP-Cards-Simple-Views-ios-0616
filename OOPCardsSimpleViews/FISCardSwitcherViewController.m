7f8f3ed68d2a0e410fc55f861b9498b5ddaa73f5
zainnadeem <zn.nadeem@gmail.com>
2016-06-16 15:18:14 -0400

    description gives you text for object

--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -3,10 +3,35 @@
 #import "FISCardSwitcherViewController.h"
 
 @interface FISCardSwitcherViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *middleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomRightLabel;
+@property (weak, nonatomic) IBOutlet UILabel *topLeftLabel;
 
 @end
 
 @implementation FISCardSwitcherViewController
+
+- (IBAction)threeOfSpadeButton:(UIButton *)sender {
+    _middleLabel.text = self.threeOfSpades.description;
+     _topLeftLabel.text = self.threeOfSpades.description;
+     _bottomRightLabel.text = self.threeOfSpades.description;
+    
+}
+- (IBAction)fourOfClubsButton:(UIButton *)sender {
+    _middleLabel.text = self.fourOfClubs.description;
+    _topLeftLabel.text = self.fourOfClubs.description;
+    _bottomRightLabel.text = self.fourOfClubs.description;
+}
+- (IBAction)eightOfDiamondsButton:(UIButton *)sender {
+    _middleLabel.text = self.eightOfDiamonds.description;
+    _topLeftLabel.text = self.eightOfDiamonds.description;
+    _bottomRightLabel.text = self.eightOfDiamonds.description;
+}
+- (IBAction)tenOfHeartsButton:(UIButton *)sender {
+    _middleLabel.text = self.tenOfHearts.description;
+    _topLeftLabel.text = self.tenOfHearts.description;
+    _bottomRightLabel.text = self.tenOfHearts.description;
+}
 
 - (void)viewDidLoad
 {
